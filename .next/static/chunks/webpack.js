6863d8d6d9d707598fc0204da682fe570846b45e
ekasupandie <eka.supandi@gmail.com>
2023-02-25 02:30:06 +0700

    update navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3885cb874ac24bc8"; }
+/******/ 		__webpack_require__.h = function() { return "b0e001a73d879814"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
